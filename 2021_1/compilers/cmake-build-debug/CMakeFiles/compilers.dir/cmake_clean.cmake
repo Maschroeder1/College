c7470101c0b9d06439067a48065304a31d48ae90
Maschroeder1 <mikaelarnez@hotmail.com>
2021-08-10 00:03:46 -0300

    partial scanner

---
+++ 2021_1/compilers/cmake-build-debug/CMakeFiles/compilers.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compilers.dir/main.c.o"
+  "compilers"
+  "compilers.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/compilers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 2021_1/compilers/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "tokens.h"
+
+int main() {
+    printf("%d", KW_CHAR);
+    return 0;
+}
